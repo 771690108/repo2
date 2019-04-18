b928574dfb8fd9c5f2e29d16057389f141cb5c77
wanqiqi <943907301@qq.com>
2019-04-18 09:15:33 +0800

    你好

---
+++ maven-day02/maven_day02_service/src/test/java/hellotest.java
@@ -0,0 +1,10 @@
+public class hellotest {
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+    }
+}
